7c240ab03119a6ff80317a7065c8eb3529ec59f6
Anil Abhange <149065554+BinaryFighter-01@users.noreply.github.com>
2025-11-11 00:36:48 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,20 @@
+import requests as re
+T  = re.get("https://tinyurl.com/zeptoai1")
+
+print(T)
+
+
+
+
+
+# Use this any links to get code in Jupyter Notebook
+
+https://tinyurl.com/zeptoai3
+https://tinyurl.com/zeptoai5
+https://tinyurl.com/zeptoai4
+https://tinyurl.com/zeptoai1
+https://tinyurl.com/zeptodb
+https://tinyurl.com/zeptoimport
+https://tinyurl.com/zeptojoin
+https://tinyurl.com/zeptomongo
+https://tinyurl.com/zeptoai2
